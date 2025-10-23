dd6634ee27bb90902a8c41c8f7377d7f260ffde0
lucas <lucasdesouzasouza57@gmail.com>
2025-10-23 15:05:27 -0300

    atualização SMariadb

--- banco.php
+++ banco.php
@@ -14,8 +14,8 @@ $sql = "SELECT * FROM tb_alunos";
 $result = $conn->query($sql);
 echo "<body style='background-color:#ADD8E6;'>";
 echo "<center>";
-echo "<h1>SWebX -  SEU NOME -   XXXX</h1>";
-echo "<img src=’https://abre.ai/banneracme’>";
+echo "<h1>SWeb1 -  Lucas & Rafael - 9596 & 7027</h1>";
+echo "<img src=’https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.uol.com.br%2Fesporte%2Fultimas-noticias%2F2022%2F04%2F21%2Fjogadores-que-poderiam-ser-como-o-latrell-spencer-de-as-branquelas.htm&psig=AOvVaw0kuB0byyVkCGTUbgJfpJo4&ust=1761328369667000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMDTgtbxupADFQAAAAAdAAAAABAE’>";
 if ($result->rowCount() > 0) {
 	echo "<table border='1' align='center'>";
 	echo "<tr><th>ID</th><th>Nome</th><th>Telefone</th></tr>";
